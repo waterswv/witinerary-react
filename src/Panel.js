8d79542ce56dfcf27c0d689874ade8be5b20b042
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-01-23 11:59:10 -0800

    Setup Panel Component ... placed Panel in left col for title section reserved for header

---
+++ src/Panel.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Panel.css';
+
+class Panel extends Component {
+  render() {
+    return (
+
+      <div className='panel'>
+        I am a Panel
+      </div>
+
+  );
+  }
+}
+
+export default Panel;
