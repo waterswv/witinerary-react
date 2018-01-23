4116eedd21d150d15e1dae600f4ddec03dc9a1fb
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-01-23 10:54:21 -0800

    Created NavBar component & setup framework for navigation with W3C Navigation tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import NavBar from './NavBar';
 
 class App extends Component {
   render() {
     return (
 
       <div className="wrapper">
-          React Component
+          <NavBar />
       </div>
 
   );
