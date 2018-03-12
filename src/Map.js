a42e83a5060f1f452fb4ce9778c1a36089cb45c8
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-03-12 12:52:17 -0700

    setup Map component and included NavBar

---
+++ src/Map.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './Map.css';
+import NavBar from './NavBar'
+
+class Map extends Component {
+  render() {
+
+
+    return (
+
+      <div className='wrapper'>
+        <NavBar />
+
+      </div>
+
+  );
+  }
+}
+
+export default Map;
