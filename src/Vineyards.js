2e11e7a93be1d9287fe39bcb5234a55444a27b49
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-03-12 15:03:25 -0700

    completed Vineyards Component ... displaying all Wineries in Map View

--- src/Vineyards.js
+++ src/Vineyards.js
@@ -2,17 +2,16 @@ import React, {Component} from 'react';
 import './Vineyards.css';
 
 class Vineyards extends Component {
+
   render() {
     return (
 
       <div className='vineyards'>
-        <a><button className="w3-button w3-large w3-circle w3-red w3-card-4">+</button></a>
-        <p className='vineyard-title'>Unti Wines</p>
-        <p className='tasting-room'>Open: Wed - Sun 10AM - 5PM</p>
+        <p className='vineyard-title'>{this.props.name}</p>
+        <p className='tasting-room'>{this.props.description}</p>
+        <p className='hours-ops'>{this.props.hours}</p>
       </div>
-
-
-  );
+    );
   }
 }
 
