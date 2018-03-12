292007c5945e6378304838787b2753e8f6bf35bf
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-03-12 14:21:51 -0700

    setup fetch for all wineries

--- src/Map.js
+++ src/Map.js
@@ -3,6 +3,19 @@ import './Map.css';
 import NavBar from './NavBar'
 
 class Map extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      wineries: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:8000/api/winery')
+      .then((response) => response.json())
+        .then((wineries) => this.setState({wineries: wineries}))
+  }
   render() {
 
 
@@ -18,3 +31,7 @@ class Map extends Component {
 }
 
 export default Map;
+// TODO:
+// TODO: Add a map function component to place MAP on a spot on the page
+// => Call data
+// TODO: Add a new winery that can be added to the map => reRender page upon delivery
