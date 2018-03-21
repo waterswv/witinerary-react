f068664ab441f5b911b6eb21200261a22c7b771b
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-03-20 18:26:56 -0700

    revised vineyard panel design basics ... 2 rows ... 5 pieces of information, including 2 click evts

--- src/Vineyards.js
+++ src/Vineyards.js
@@ -7,9 +7,16 @@ class Vineyards extends Component {
     return (
 
       <div className='vineyards'>
-        <p className='vineyard-title'>{this.props.name}</p>
-        <p className='tasting-room'>{this.props.description}</p>
-        <p className='hours-ops'>{this.props.hours}</p>
+        <div className='heading'>
+          <p className='vineyard-title'>{this.props.name}</p>
+          <p className='tasting-room'><a href='#'> {'More Info'}</a></p>
+          <p className='add-winery'><a href='#'>{'Click Here to Add Me'}</a></p>
+        </div>
+        <div className='heading'>
+          <p className='vineyard-address'>{'123 Main St. Healdsburg, CA'}</p>
+          <p className='more-info'></p>
+          <p className='hours-ops'>{this.props.hours}</p>
+        </div>
       </div>
     );
   }
